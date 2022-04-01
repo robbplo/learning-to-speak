bfefb357168c928e44bc921ea3642ca1118de975
Robbin Ploeger <robbplo@gmail.com>
2022-04-01 15:28:08 +0200

    Order tasks by created

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,7 +8,7 @@ class TaskController extends Controller
 {
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::orderBy('created_at', 'desc')->get();
         return view('tasks', compact('tasks'));
     }
 
